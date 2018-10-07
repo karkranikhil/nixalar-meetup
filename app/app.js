ae599623a71f49b6e2fbdd86f3b96678cd1aa8b0
karkran <nikhil.karkra@anz.com>
2018-10-07 18:53:13 +1100

    setting up json data to show up on screen

--- app/app.js
+++ app/app.js
@@ -1,11 +1,26 @@
 /*** creating server using express ***/
 var express = require('express');
 var app = express();
+var dataFile = require('./data/data.json')
+
+/*set port number based on environment variable */
+app.set('port', process.env.PORT || 3000);
 app.get('/',(req, res)=>{
-    res.send('<h1>Nixalar meetup</h1>')
+    var info = '';
+    dataFile.speakers.forEach(item => {
+        info +=`
+        <li>
+            <h2>${item.name}</h2>
+            <p>${item.summary}</p>
+        </li>
+        `
+    });
+    res.send(`
+    <h1>Nixalar meetup</h1>
+    ${info}`)
 })
 
-var server = app.listen(3000, ()=>console.log('go to http://localhost:3000 on your browser'))
+var server = app.listen(app.get('port'), ()=>console.log('go to http://localhost:'+app.get('port')+ ' on your browser'))
 
 
 
